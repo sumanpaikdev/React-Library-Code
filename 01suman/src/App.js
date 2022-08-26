1c3b5b149cc158c79b06b1496f70bd2fdf50153e
Suman Paik <sumanpaikcs@gmail.com>
2022-08-26 19:44:51 +0530

    props added

---
+++ 01suman/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+   <>
+   {/* <Navbar title="FirstReact 1.0"  abouttext="About 1.0s" /> */}
+   <Navbar />
+   </>
+  );
+}
+
+export default App;
