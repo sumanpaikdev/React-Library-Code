c6caaf4dd3a84614deb5b2afabb18e2e750a9462
Suman Paik <sumanpaik.dev@gmail.com>
2022-10-06 12:10:56 +0530

    yes no modified

--- yes-or-no/src/components/Main.js
+++ yes-or-no/src/components/Main.js
@@ -1,19 +1,29 @@
 import React from "react"
 
 export default function Main() {
-    const [text, setText] = React.useState("Yes")
+    // const [text, setText] = React.useState("Yes")
 
 
-    function changeContent() {
-        setText("No")
+    // function changeContent() {
+    //     setText("No")
 
+    // }
+
+
+    // yes no toggle
+
+    const [toggleItem, setToggleItem] = React.useState(true)
+    function changeItem() {
+        setToggleItem(prevstate => !prevstate)
+        
     }
+
 
     return (
         <div className="main--container">
             <h2>Is React Important?</h2>
-            <div className="yes--no" onClick={changeContent}>
-                <h4>{text}</h4>
+            <div className="yes--no" onClick={changeItem}>
+                <h4>{toggleItem ? "Yes" : "No"}</h4>
             </div>
         </div>
     )
