0e764522e348ee3fb792d5ec2ce1213ac2686765
Suman Paik <sumanpaik.dev@gmail.com>
2022-10-28 11:00:00 +0530

    arrow function toggle with normal function when map the data from the data.js file

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -8,24 +8,30 @@ import world from "./images/world.png"
 import pointer from "./images/placeholder.png"
 
 function App() {
-  const dataset = Data.map((itemDetails) => {
-    return (
-      <Place
-        point={pointer}
-        details={itemDetails}
-      />
-    )
+  // const dataset = Data.map((Data) => {                  //This is Arrow function
+  //   return (
+  //     <Place
+  //       point={pointer} 
+  //       details={Data}
+  //     />
+  //   );
 
-  })
+  // })
+  const dataset = Data.map(myDataFunction)
+  function myDataFunction(elementItems) {
+    return(
+      <Place
+        point = {pointer}
+        details = {elementItems}
+       />
+    );
+  }
   return (
     <div className='react--app--container'>
       <div className="nav--cont--bar">
-
         <Navbar img={world} />
       </div>
       <div className="app--container">
-
-
         {dataset}
       </div>
     </div>
